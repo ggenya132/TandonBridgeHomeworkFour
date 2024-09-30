cd1bb42d70b0a8badc5e962778e8e9c24d179d9d
ggenya132 <eugene.vedensky@gmail.com>
2024-09-29 22:34:25 -0400

    comitting homework again

--- ev2361_hw4_q6.cpp
+++ ev2361_hw4_q6.cpp
@@ -1,43 +1,37 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
 
-double nPower(int integer, double degreeOfPower){
-    return pow(integer, (1.0/degreeOfPower));
-};
+bool hasMoreEvenThanOddDigits(int integer){
 
+   string integerToString = to_string(integer);
+   int numberOfEvens =0 ,numberOfOdds = 0;
+
+   for(int i =0; i < integerToString.length(); i++){
+    int currentInteger = integerToString[i];
+
+    if(currentInteger % 2 == 0){
+        numberOfEvens++;
+    }
+    else {
+        numberOfOdds++;
+    }
+   }
+
+   return numberOfEvens > numberOfOdds;
+
+};
 
 int main(){
     
-    double lengthOfIterativeSequence; 
-    double resultOfIterativeSequence = 1;
+    int targetInteger;
 
-    cout<<"Please enter the length of the sequence: ";
-    cin>>lengthOfIterativeSequence;
-    cout<<"Please enter your sequence: "<<endl;
-
-    double currentIntegerInSequence; 
-
-    for(int i = 0 ; i < lengthOfIterativeSequence; i++){
-        cin>>currentIntegerInSequence;
-        resultOfIterativeSequence *= currentIntegerInSequence;
-    }
-
-    cout<<"The geometric mean is: "<<nPower(resultOfIterativeSequence, lengthOfIterativeSequence)<<endl;
-
-    cout<<"Please enter a non-empty sequence of positive integers, each one in a separate line. End your sequence by typing -1:"<<endl;
-
-
-    int enteredNumber = 1;
-    double numberOfDigitsEntered = 0;
-    int result = 1;
-    while(enteredNumber!= -1){
-        cin>>enteredNumber;
-        if(enteredNumber!=-1){
-            result *= enteredNumber;
-            numberOfDigitsEntered++;
+    cout<<"Enter positive integer: "<<endl;
+    cin>>targetInteger;
+    for(int currentInteger = 1; currentInteger <= targetInteger; currentInteger++){
+        if(hasMoreEvenThanOddDigits(currentInteger)){
+            cout<<currentInteger<<endl;
         }
     }
-    cout<<"The geometric mean is: "<<nPower(result, numberOfDigitsEntered)<<endl;
+    return -1;
 }
